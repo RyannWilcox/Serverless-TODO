1386995c04c5e54bea6641dd3af12b61073b691c
Ryan Wilcox <rwilcox11@icloud.com>
2021-11-28 11:54:14 -0500

    Fixed some status codes and removed unneeded cors data

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -5,20 +5,16 @@ import { createLogger } from '../utils/logger'
 const XAWS = AWSXRay.captureAWS(AWS)
 const logger = createLogger('AttachmentUtils')
 
-// TODO: Implement the fileStogare logic
-
 export class AttachmentUtils {
   constructor (
     private readonly s3 = new XAWS.S3({signatureVersion: 'v4'}),
-    private readonly bucket = process.env.ATTACHMENT_S3_BUCKET,
-    //private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION
-    ){}
+    private readonly bucket = process.env.ATTACHMENT_S3_BUCKET){}
 
     async getUploadUrl(todoId: string) : Promise<string>{
       return this.s3.getSignedUrl('putObject',{
         Bucket: this.bucket,
         Key: todoId,
-        Expires: 3000
+        Expires: 300
       })
 
     }
--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,9 +1,7 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
-//import { getUserId } from '../utils'
 import { createLogger } from '../../utils/logger'
 import { createAttachmentPresignedUrl, updateAttachmentUrl } from '../../helpers/todos'
 
@@ -11,14 +9,13 @@ const logger = createLogger('generateUploadUrl')
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
     logger.info('Received event => ', event)
-    
+    const todoId = event.pathParameters.todoId
     await createAttachmentPresignedUrl(todoId)
     const attachUrl = await updateAttachmentUrl(todoId)
     
     return {
-      statusCode: 201,
+      statusCode: 200,
       body: JSON.stringify({
         uploadUrl: attachUrl
       })
